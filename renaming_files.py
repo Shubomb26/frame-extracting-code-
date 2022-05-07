50d8595c627e51f518cdc3209125836166f25617
Shubomb26 <96885144+Shubomb26@users.noreply.github.com>
2022-05-07 11:39:08 +0530

    Add files via upload

---
+++ renaming_files.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 10:29:28 2020
+
+@author: akash
+"""
+
+
+import os 
+  
+# Function to rename multiple files 
+def main(): 
+  
+    for count, filename in enumerate(os.listdir('C:\\Users\\singh\\OneDrive\\Desktop\\exact images\\')):
+        dst ="tencent_" + str(count) + ".jpg"
+        src ='C:\\Users\\singh\\OneDrive\\Desktop\\exact images\\'+ filename
+        dst ='C:\\Users\\singh\\OneDrive\\Desktop\\exact images\\img_'+ dst
+          
+        # rename() function will 
+        # rename all the files 
+        os.rename(src, dst) 
+  
+# Driver Code 
+if __name__ == '__main__': 
+      
+    # Calling main() function 
+    main() 
